6a4e1eca88d46d0e7252b877ae80340aba02f83e
pags666 <pags40502@gmail.com>
2026-03-17 07:51:36 +0530

    Update monc.py

--- monc.py
+++ monc.py
@@ -33,7 +33,7 @@ for page in range(1, pages + 1):
         if h2 and a:
 
             title = h2.text.strip()
-            link = a.get("href")
+       
 
             rows.append([title, link])
 
@@ -44,7 +44,7 @@ for page in range(1, pages + 1):
 SHEET_ID = "1le7tQxVkznMvphgOB2T0tGyzb_ByeaOHJ4R9E5piY_A"
 WORKSHEET = "monc"
 
-headers = ["TITLE", "LINK"]
+headers = ["TITLE"]
 
 # push to Google Sheets
 update_google_sheet_by_name(SHEET_ID, WORKSHEET, headers, rows)
